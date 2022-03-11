20be6c610beedcfa6723eeff1c0d582476243c5f
TalCohenHarari <74271133+TalCohenHarari@users.noreply.github.com>
2022-03-11 13:01:35 +0200

    List by LazyColumn

--- app/src/main/java/com/composeproject/MainActivity.kt
+++ app/src/main/java/com/composeproject/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
@@ -19,13 +21,12 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val scrollState = rememberScrollState()
-            Column(
-                modifier = Modifier.verticalScroll(scrollState)
-            ) {
-                for (i in 1..25) {
+            LazyColumn {
+                itemsIndexed(
+                    listOf("This", "is", "jetpack", "jetpack", "compose")
+                ) { index, item ->
                     Text(
-                        text = "Item $i",
+                        text = item,
                         fontSize = 24.sp,
                         textAlign = TextAlign.Center,
                         fontWeight = FontWeight.Bold,
@@ -34,9 +35,25 @@ class MainActivity : ComponentActivity() {
                             .padding(vertical = 24.dp),
                     )
                 }
+//                items(5000) {
+//                    Text(
+//                        text = "Item $it",
+//                        fontSize = 24.sp,
+//                        textAlign = TextAlign.Center,
+//                        fontWeight = FontWeight.Bold,
+//                        modifier = Modifier
+//                            .fillMaxWidth()
+//                            .padding(vertical = 24.dp),
+//                    )
+//                }
             }
         }
     }
 }
-
-/** LazyColumn/Row is the equivalent to recyclerView*/
+/** LazyColumn/Row is the equivalent to recyclerView.
+ * LazyColumn already scrollable by default.
+ * "Column" are loading all the list in one time, "LazyColumn" load the list exact like RecyclerView.
+ * "items" = loop our items (need to give the size list) and the index is "it"
+ * [itemsIndexed gives us the index and the item].
+ *
+ * */
