8ddbdfc49907509cef7e11be1f50b23f2a849eed
TalCohenHarari <74271133+TalCohenHarari@users.noreply.github.com>
2022-03-12 20:51:52 +0200

    SimpleAnimations

--- app/src/main/java/com/composeproject/MainActivity.kt
+++ app/src/main/java/com/composeproject/MainActivity.kt
@@ -3,87 +3,58 @@ package com.composeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val painter = painterResource(id = R.drawable.pikachu)
-            val title = "My name is Pikachu"
-            val description = "POKEMON"
+            var sizeState by remember { mutableStateOf(200.dp) }
+            //This "animateDpAsState" will increase the size in animation style
+            val size by animateDpAsState(
+                targetValue = sizeState, //startValue
+                //animationSpec:
+                tween(
+                    durationMillis = 1000, // Animate in all this time
+//                    delayMillis = 300, //Start after this delay
+//                    easing = LinearOutSlowInEasing
+                )
+//                spring(
+//                    Spring.DampingRatioHighBouncy
+//                )
+            )
+            val infiniteTransition = rememberInfiniteTransition()
+            val color by infiniteTransition.animateColor(
+                initialValue = Color.Red,
+                targetValue = Color.Green,
+                animationSpec = infiniteRepeatable(
+                    tween(durationMillis = 2000),
+                    repeatMode = RepeatMode.Reverse
+                )
+            )
             Box(
                 modifier = Modifier
-                    .fillMaxWidth(0.5f)
-                    .padding(16.dp)
+                    .size(size)
+                    .background(color),
+                contentAlignment = Alignment.Center
             ) {
-                ImageCard(
-                    painter = painter,
-                    title = title,
-                    contentDescription = description
-                )
-            }
-        }
-    }
-}
+                Button(onClick = {
+                    sizeState += 50.dp
+                }) {
+                    Text(text = "Increasere Size")
+                }
 
-@Composable
-fun ImageCard(
-    painter: Painter,
-    title: String,
-    contentDescription: String,
-    modifier: Modifier = Modifier
-) {
-    Card(
-        modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(15.dp),
-        elevation = 15.dp
-    ) {
-        Box(modifier = Modifier.height(200.dp)) {
-            Image(
-                painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Crop
-            )
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(
-                        Brush.verticalGradient(
-                            colors = listOf(
-                                Color.Transparent,
-                                Color.Black
-                            ),
-                            startY = 300f
-                        )
-                    )
-            )
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(12.dp),
-                contentAlignment = Alignment.BottomCenter
-            ) {
-                Text(
-                    text = title,
-                    style = TextStyle(Color.White, fontSize = 16.sp)
-                )
             }
         }
     }
