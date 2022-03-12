abe0398b621abbe119bde08582fbb05e7617386c
TalCohenHarari <74271133+TalCohenHarari@users.noreply.github.com>
2022-03-12 20:29:26 +0200

    SideEffects + EffectsHandlers + produceState

--- app/src/main/java/com/composeproject/MainActivity.kt
+++ app/src/main/java/com/composeproject/MainActivity.kt
@@ -2,14 +2,15 @@ package com.composeproject
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.OnBackPressedDispatcher
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -20,71 +21,59 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val painter = painterResource(id = R.drawable.pikachu)
-            val title = "My name is Pikachu"
-            val description = "POKEMON"
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth(0.5f)
-                    .padding(16.dp)
-            ) {
-                ImageCard(
-                    painter = painter,
-                    title = title,
-                    contentDescription = description
-                )
+            val scaffoldState = rememberScaffoldState()
+            val scope = rememberCoroutineScope()
+            Scaffold(scaffoldState = scaffoldState) {
+                val counter = produceState(initialValue = 0){
+                    delay(3000L)
+                    value = 4
+                }
+                if(counter.value % 5 == 0 && counter.value > 0){
+                    LaunchedEffect(key1 = scaffoldState.snackbarHostState){
+                        scaffoldState.snackbarHostState.showSnackbar("Hello")
+                    }
+                }
+                Button(onClick = {}) {
+                    Text(text = "Click me: ${counter.value}")
+                }
             }
         }
     }
 }
 
+var i = 0
+
 @Composable
-fun ImageCard(
-    painter: Painter,
-    title: String,
-    contentDescription: String,
-    modifier: Modifier = Modifier
-) {
-    Card(
-        modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(15.dp),
-        elevation = 15.dp
-    ) {
-        Box(modifier = Modifier.height(200.dp)) {
-            Image(
-                painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Crop
-            )
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(
-                        Brush.verticalGradient(
-                            colors = listOf(
-                                Color.Transparent,
-                                Color.Black
-                            ),
-                            startY = 300f
-                        )
-                    )
-            )
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(12.dp),
-                contentAlignment = Alignment.BottomCenter
-            ) {
-                Text(
-                    text = title,
-                    style = TextStyle(Color.White, fontSize = 16.sp)
-                )
+fun MyComposable(backDispatcher: OnBackPressedDispatcher){
+
+    // "remember" will create this callback once
+    val callback = remember{
+        object: OnBackPressedCallback(true){
+            override fun handleOnBackPressed() {
+                // Do something
             }
         }
+    }
+
+    // Sometimes we have code that request cleanup (free some memory or resources etc..)  after composable finished composing, so we should use "DisposableEffect"
+    DisposableEffect(key1 = backDispatcher){
+        backDispatcher.addCallback(callback)
+        onDispose {
+            callback.remove()
+        }
+    }
+
+    //This will execute just if the recompose was success, otherwise this block wil not executed
+    SideEffect {
+        i++
+    }
+    Button(onClick = {}) {
+        Text(text = "Click Me")
     }
 }
