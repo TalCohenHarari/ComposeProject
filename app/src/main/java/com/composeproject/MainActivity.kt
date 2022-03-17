90c8d64cbdc18d3811f0a276002551c37ac9d895
TalCohenHarari <74271133+TalCohenHarari@users.noreply.github.com>
2022-03-17 20:38:24 +0200

    Animated circular ProgressBar

--- app/src/main/java/com/composeproject/MainActivity.kt
+++ app/src/main/java/com/composeproject/MainActivity.kt
@@ -1,24 +1,32 @@
 package com.composeproject
 
-import android.media.Rating
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
@@ -28,11 +36,53 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Box(
-                contentAlignment = Alignment.Center,
-                modifier = Modifier.fillMaxSize()
-            ) {
-                CircularProgressBar(percentage = 0.8f, number = 100)
+            MyContent()
+        }
+    }
+
+    @Composable
+    private fun MyContent() {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black)
+        ) {
+            Column {
+                Text(
+                    text = "Monthly Statistic",
+                    fontSize = 26.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    color = Color.White,
+                    textDecoration = TextDecoration.Underline,
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                )
+                Spacer(modifier = Modifier.height(50.dp))
+                Row {
+                    CircularProgressBar(percentage = 0.8f, number = 100)
+                    Spacer(
+                        modifier = Modifier
+                            .height(100.dp)
+                            .width(100.dp)
+                    )
+                    CircularProgressBar(percentage = 0.95f, number = 100, color = Color.Red)
+                }
+                Spacer(
+                    modifier = Modifier
+                        .height(100.dp)
+                        .width(100.dp)
+                )
+                Row {
+                    CircularProgressBar(percentage = 1f, number = 100, color = Color.Yellow)
+                    Spacer(
+                        modifier = Modifier
+                            .height(100.dp)
+                            .width(100.dp)
+                    )
+                    CircularProgressBar(percentage = 0.76f, number = 100, color = Color.Blue)
+                }
+                Spacer(modifier = Modifier.height(40.dp))
             }
         }
     }
@@ -81,9 +131,56 @@ fun CircularProgressBar(
         }
         Text(
             text = (currentPercentage.value * number).toInt().toString(),
-            color = Color.Black,
+            color = Color.White,
             fontSize = fontSize,
             fontWeight = FontWeight.Bold
         )
+    }
+}
+
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    title: String,
+    contentDescription: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 15.dp
+    ) {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = 300f
+                        )
+                    )
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomCenter
+            ) {
+                Text(
+                    text = title,
+                    style = TextStyle(Color.White, fontSize = 16.sp)
+                )
+            }
+        }
     }
 }
